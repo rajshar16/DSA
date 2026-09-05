21d0e56373e224dd3a2ff16f63522290a0c5d651
rajshar16 <rajsharma09022004@gmail.com>
2026-09-05 13:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 76.5 MB (98.46%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,34 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //create new link list
-        ListNode* dummyhead=new ListNode(0);
-        //point the new linklist
-        ListNode* current=dummyhead;
-          int carry=0;
-
-
-          // Continue if there are nodes left in either list, OR if there's a leftover carry
-        while(l1 != nullptr  || l2 !=nullptr || carry!=0) {
-          int x= (l1!= nullptr)? l1-> val :0;
-          int y= (l2!= nullptr)? l2-> val :0;
-
-
-        int digit=x +y +carry;
-        carry=digit/10;
-
-
-        // Create a new node with the current digit
-        current->next = new ListNode(digit % 10);
-            current = current->next;
-// Advance the pointers if they aren't null
-            if (l1 != nullptr) l1 = l1->next;
+        ListNode* head = l1;
+        ListNode* prev = nullptr;
+        int carry = 0;
+        
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            carry = sum / 10;
+            
+            if (l1 != nullptr) {
+                l1->val = sum % 10;
+                prev = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = new ListNode(sum % 10);
+                prev = prev->next;
+            }
+            
             if (l2 != nullptr) l2 = l2->next;
         }
         
-        ListNode* result = dummyhead->next;
-        delete dummyhead; // Free the dummy node memory
-        return result;
-        
+        return head;
     }
 };

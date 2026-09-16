f8be367d0db76dde1c8767f9625f114fd1c640e5
rajshar16 <rajsharma09022004@gmail.com>
2026-09-16 23:46:56 +0530

    Time: 7 ms (95.34%) | Memory: 58.1 MB (67.38%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int>arr;
+        ListNode* curr=head;
+        while(curr){
+            arr.push_back(curr->val);
+            curr=curr->next;
+        }
+        sort(arr.begin(),arr.end());
+        curr=head;
+        int index=0;
+        while(curr){
+            curr->val=arr[index++];
+            curr=curr->next;
+        }
+        return head;
+    }
+};

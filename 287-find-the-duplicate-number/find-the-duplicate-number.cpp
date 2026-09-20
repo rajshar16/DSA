b6d184191ed75257567df7ff69391f25abe018d0
rajshar16 <rajsharma09022004@gmail.com>
2026-09-21 00:22:14 +0530

    Time: 23 ms (36.47%) | Memory: 65.1 MB (41.39%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mpp; 
+        int low = 1;
+        int high = nums.size() - 1; // Since array size is n + 1, max value is n
         
-        for (int num : nums) {
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
             
-            if (mpp.count(num) > 0) {
-                return num;
+            // Count how many numbers are less than or equal to mid
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
             }
             
-            mpp[num]++;
+            // If count exceeds mid, the duplicate is in the lower half
+            if (count > mid) {
+                high = mid;
+            } 
+            // Otherwise, it's in the upper half
+            else {
+                low = mid + 1;
+            }
         }
         
-        return -1; 
+        // low and high converge to the duplicate number
+        return low; 
     }
 };

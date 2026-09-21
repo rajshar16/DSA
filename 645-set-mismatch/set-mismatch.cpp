52b7432d7ba0373212aa38c393d38f2f478d5a84
rajshar16 <rajsharma09022004@gmail.com>
2026-09-21 23:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (55.32%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n=nums.size();
-    vector<int>arr;
-vector<int>hash(n+1,0);
-    for(int i=0;i<nums.size();i++){
-        hash[nums[i]]++;
-    }
-    int y,k;
-    for(int i=1;i<=n;i++){
-        if(hash[i]==0){
-            k=i;}
-            if(hash[i]==2){
-                y=i;
+        int n = nums.size();
+        
+        
+        int hash[10005] = {0}; 
+        
+        for(int i = 0; i < n; i++){
+            hash[nums[i]]++; 
+        }
+        
+        int y, k;
+        for(int i = 1; i <= n; i++){
+            if(hash[i] == 0){
+                k = i; 
             }
+            if(hash[i] == 2){
+                y = i; 
+            }
+        }
+     return {y, k}; 
     }
-    arr.push_back(y);
-    arr.push_back(k);
-return arr;
-    }
-};
+};   
+        
